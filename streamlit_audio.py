415e14852f4262225e08f18fdfb8d7d9f462a92f
xuying li <xuyingli.kepler@gmail.com>
2023-09-28 17:33:14 +0800

    Update streamlit_audio.py

--- streamlit_audio.py
+++ streamlit_audio.py
@@ -368,7 +368,7 @@ def input_page(st, **state):
     """, 
     unsafe_allow_html=True
     )
-    st.markdown("<h3 style='text-align: center; color: black;'>Empower Your Day with Real-Time Insights: Leveraging AI for Instant News and Podcast Updates.</h2>", unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align: center; color: black;'>Empower Your Day with Real-Time Insights: Leveraging AI for Instant News <br> and Podcast Updates.</h2>", unsafe_allow_html=True)
 
     st.markdown("""
         <h4 style='text-align: center; color: gray;'>
