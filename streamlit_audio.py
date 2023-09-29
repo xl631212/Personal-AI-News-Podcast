7eaffed40aaa06dd6d5ac6aafa6e703f9680494e
xuying li <xuyingli.kepler@gmail.com>
2023-09-30 01:35:03 +0800

    Update streamlit_audio.py

--- streamlit_audio.py
+++ streamlit_audio.py
@@ -678,9 +678,7 @@ def compute_page(st, **state):
                     {'role':'user',
                     'content': f"【{query}】"},]
     response = get_completion_from_messages(messages)
-
     my_bar.progress(90, text="Generating Podcast...")
-
     if st.session_state.language == 'English':
         updated_text = response
         # 构建 edge-tts 命令
